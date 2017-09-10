f979b6323894368ab8c3a3d41c7bcd0a3f45942a
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-09-09 19:28:28 -0700

    refactor video promises

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
+const _ = require('lodash');
 
 const {app, BrowserWindow, ipcMain} = electron;
 
@@ -17,12 +18,12 @@ app.on('ready', () => {
 });
 
 ipcMain.on('videos:added', (event, videos) => {
-  const promise = new Promise((resolve, reject) => {
-    ffmpeg.ffprobe(videos[0].path, (err, metadata) => {
-      resolve(metadata);
-    });
-  });
-  promise.then((metadata) => {
-    console.log(metadata)
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        resolve(metadata);
+      })
+    })
   })
+  Promise.all(promises).then((results) => results)
 });
